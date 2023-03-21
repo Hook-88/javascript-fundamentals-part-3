8ea0950f48bcaa2b1beeef66de7cdbb19743e1bc
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-21 13:24:40 -0500

    Add capitalize function

---
+++ script.js
@@ -0,0 +1,27 @@
+const add7 = (number) => {
+  return number + 7;
+}
+
+const multiply = (x, y) => {
+  return x * y;
+}
+
+const getFirstChar = (string) => {
+  return string.substr(0,1);
+}
+
+const getTheRestOfString = (string) => {
+  return string.substr(1);
+}
+
+const capitalize = (string) => {
+  let firstChar = getFirstChar(string).toUpperCase();
+  let restOfString = getTheRestOfString(string).toLowerCase();
+  return `${firstChar}${restOfString}`;
+  
+}
+
+console.log(add7(7));
+console.log(multiply(3,3));
+console.log(capitalize('heLLo'));
+
